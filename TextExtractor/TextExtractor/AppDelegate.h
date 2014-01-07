dec1e4368c62b37e745f616359917406519106ff
vimal.m <vimal.m@gofrugal.com>
2014-01-07 11:20:23 +0530

    initial commit

---
+++ TextExtractor/TextExtractor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TextExtractor
+//
+//  Created by admin on 07/01/14.
+//  Copyright (c) 2014 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
